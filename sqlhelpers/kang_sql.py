bfcd410b4f15126931f153def4dab734c8de8a29
your_username <43999863+Priyam005@users.noreply.github.com>
2020-06-25 10:34:41 +0530

    bruh #2

--- sqlhelpers/kang_sql.py
+++ sqlhelpers/kang_sql.py
@@ -4,17 +4,17 @@ from sqlalchemy import Column, Integer
 
 class Pack(BASE):
     __tablename__ = "pack"
-    id = Column(Integer, primary_key=True, default=1)
+    pack_id = Column(Integer, primary_key=True, default=1)
 
-    def __init__(self, id):
-        self.id = int(id)
+    def __init__(self, pack_id):
+        self.pack_id = int(pack_id)
 
 
 Pack.__table__.create(checkfirst=True)
 
 
 def get_id():
-    return SESSION.query(Pack).get(int(id))
+    return SESSION.query(Pack).get(int(pack_id))
     SESSION.close()
 
 
